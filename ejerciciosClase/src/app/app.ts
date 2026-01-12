35f87edca19fca80a15c2d40c88def2890264c61
Diego <riveradiegoyancha@gmail.com>
2026-01-12 18:40:25 +0100

    dsada

--- ejerciciosClase/src/app/app.ts
+++ ejerciciosClase/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { UserProfile } from './user-profile/user-profile';
 import { TextStyle } from './text-style/text-style';
 import { StatusRow } from './status-row/status-row';
+import { HoverButton } from './hover-button/hover-button';
 
 @Component({
   selector: 'app-root',
-  imports: [UserProfile, TextStyle,StatusRow],
+  imports: [UserProfile, TextStyle, StatusRow, HoverButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ ejerciciosClase/src/app/hover-button/hover-button.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgClass } from "../../../node_modules/@angular/common/types/_common_module-chunk";
+
+@Component({
+  selector: 'app-hover-button',
+  imports: [NgClass],
+  templateUrl: './hover-button.html',
+  styleUrl: './hover-button.css',
+})
+export class HoverButton {
+
+}
