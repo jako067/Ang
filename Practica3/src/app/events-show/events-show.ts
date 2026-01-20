328103c27ae58c678ad55bd8c56d36d557bb5a40
Diego <riveradiegoyancha@gmail.com>
2026-01-20 18:42:58 +0100

    dsad

---
+++ Practica3/src/app/events-show/events-show.ts
@@ -0,0 +1,44 @@
+import { Component, Pipe } from '@angular/core';
+import {IEvent} from '../interfaces/i-event';
+import { CurrencyPipe, DatePipe, NgStyle } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-events-show',
+  imports: [TitleCasePipe, DatePipe,CurrencyPipe, FormsModule],
+  templateUrl: './events-show.html',
+  styleUrl: './events-show.css',
+})
+export class EventsShow {
+
+  events: IEvent[]=[
+    {
+    title: 'cumple',
+    image: 'minecraft.jpg',
+    date: '2025-11-29',
+    description: 'celebración del día en el que se llega al mundo',
+    price: 10,
+    },
+    {    
+    title: 'navidad',
+    image: 'herobrine.jpg',
+    date: '2025-11-30',
+    description: 'celebración del día en el que jesus nació',
+    price: 100,
+    },
+
+  ];
+
+  orderDate(){
+
+    
+  }
+    orderPrice(){
+    
+
+  }
+  search: '',
+
+}
